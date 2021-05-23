74476d75b19dcfa793c240480401e2f57a4d6428
Rowan Mulder <102707@student.horizoncollege.nl>
2021-05-24 01:18:35 +0200

    Toegevoegd van toegang naar Breeze pagina's

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
+// Authenticatie voor beheerders
+require __DIR__ . '/auth.php';
+Route::view('/dashboard', 'dashboard')->middleware(['auth'])->name('dashboard');
+
 });
